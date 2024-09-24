79a2e3d758874d6fb787098a169c0215fe7e35ac
sanDaws <castrogiraldosantiago@gmail.com>
2024-09-24 16:26:16 -0500

    fix: implementation and definition diagrams

--- src/js/Diagrams/GetData.js
+++ src/js/Diagrams/GetData.js
@@ -3,7 +3,10 @@ let endpoint= "https://zenny.azurewebsites.net"
 export async function GetData(totalEmpoint) {// get activity from
     
     let respone= await fetch(endpoint+totalEmpoint)
+    if (respone.status === 404) {
+        return 0
+    }
     let data=await respone.json()
-    console.log(typeof data)
+
     return data
 }
